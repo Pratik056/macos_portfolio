0ea300fa230da7f9758fba1e5e3cf929c3d5a265
Pratik056 <pratiktamrakar56@gmail.com>
2026-02-12 09:06:52 +0545

    dock

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Welcome from "#components/Welcome";
+import Navbar from "#components/Navbar";
+import Dock from "#components/Dock";
+
+export { Navbar, Welcome, Dock };
---
+++ src/store/Window.js
@@ -0,0 +1,30 @@
+import { WINDOW_CONFIG } from '#constants';
+import { create } from 'zustand';
+
+const useWindowStore = create(immer((set) => ({
+    windows: WINDOW_CONFIG,
+        nextZIndex: INITIAL_Z_INDEX + 1,
+
+        openWindow: (windowKey, data = null) => set ((state) =>{
+            const win = state.windows[windowKey];
+            win.isOpen = true;
+            win.zIndex = state.nextZIndex;
+            win.data = data ?? win.data;
+            state.nextZIndex++;
+        }),
+
+        closeWindow: (windowKey) => set ((state) => {
+            const win = state.windows[windowKey];
+            win.isOpen =false;
+            win.zIndex = state.nextZIndex;
+            win.data = null;
+        }),
+
+        focusWindow: (windowKey) => set ((state) => {
+            const win = state.windows[windowKey];
+            win.zIndex = state.nextZIndex++;
+        }),
+    }))
+);
+
+export default useWindowStore;
